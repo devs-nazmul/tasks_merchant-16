10a343ac5ca486793bba26fed7b9518fb5c5b955
Nazmul Hossain <Belivup@gmail.com>
2024-05-15 18:30:25 +0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,24 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+
+};
+
+export const metadata = {
   title: "Merchant 16",
   description: "Merchant Ecommerce",
-};
+  keywords: ["Merchant"],
+  authors: {name: "Nazmul Hossain", url: "https://dev.nazmul.co/" },
+  metadataBase: new URL('https://Merchant.com'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US'},
+  },
+  openGraph: {
+    images: '/og_image.png',
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
